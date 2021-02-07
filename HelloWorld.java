8397d8369764c70568fdbe286fbb1f3e8f17f88f
Sahothron <70182294+Sahothron@users.noreply.github.com>
2021-02-06 19:12:21 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+Author: Zach Hess
+Data: 2/6/21
+Description: A Hello World Sample
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
